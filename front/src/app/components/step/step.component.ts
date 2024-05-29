e65ff7f018bf685910be7fe8ed8e0ebde9db99e0
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-05-29 22:46:12 +0200

    feat: Add new components for home, step, tutorial, and not-found

---
+++ front/src/app/components/step/step.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './step.component.html',
+  styleUrl: './step.component.css'
+})
+export class StepComponent {
+
+}
