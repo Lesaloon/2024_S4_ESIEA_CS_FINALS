3351d5b6742e1126b3d63d572b2e67232b03d338
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-05-29 22:16:37 +0200

    chore: Add ApiService and HttpClientService for API communication

---
+++ front/src/app/services/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClientService } from './http-client.service';
+import { Tutorial } from '../interfaces/tutorial';
+import { ApiResponse } from '../api-response';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private HttpClientService: HttpClientService) { }
+
+  getAllTutorials() : Tutorial[] {
+    return this.HttpClientService.get<Tutorial>('tutorials/all')
+  }
+
+  getTutorial(id: number) : Tutorial{
+    // there is only one element in the array, its an array only for the sake of the interface
+    return this.HttpClientService.get<Tutorial>('tutorials/' + id)[0];
+  }
+
+  addTutorial(tutorial: Tutorial) {
+    return this.HttpClientService.post('tutorials/add', tutorial);
+  }
+
+  updateTutorial(tutorial: Tutorial) {
+    const id = tutorial.id;
+    return this.HttpClientService.put('tutorials/update/' + id, tutorial);
+  }
+}
---
+++ front/src/app/services/http-client.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { ApiResponse } from '../api-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  static URL = "http://localhost:8080/tutoapi/api/"
+
+  constructor(@Inject(HttpClient) private http: HttpClient) { }
+
+  // warning : this way not work, since the result is returned before the data is fetched so it may be empty.
+  //
+  get<T>(url: string): Observable<T[]> {
+    return this.http.get(URL + url).pipe(
+      map((response: Object) => {
+        const apiResponse = response as ApiResponse<T>;
+        if (apiResponse.success) {
+          return apiResponse.payload;
+        } else {
+          console.error(apiResponse.message);
+          console.error(apiResponse.errors);
+          return [];
+        }
+      })
+    );
+  }
+
+  post<T>(url: string, data: any) : Observable<T[]> {
+    return this.http.post( URL + url, data).pipe(
+      map((response: Object) => {
+        const apiResponse = response as ApiResponse<T>;
+        if (apiResponse.success) {
+          return apiResponse.payload;
+        } else {
+          console.error(apiResponse.message);
+          console.error(apiResponse.errors);
+          return [];
+        }
+      })
+    );
+  }
+
+  put<T>(url: string, data: any) : Observable<T[]> {
+    return this.http.put( URL + url, data).pipe(
+      map((response: Object) => {
+        const apiResponse = response as ApiResponse<T>;
+        if (apiResponse.success) {
+          return apiResponse.payload;
+        } else {
+          console.error(apiResponse.message);
+          console.error(apiResponse.errors);
+          return [];
+        }
+      })
+    );
+  }
+
+  delete<T>(url: string)  : Observable<T[]>  {
+    return this.http.delete( URL + url).pipe(
+      map((response: Object) => {
+        const apiResponse = response as ApiResponse<T>;
+        if (apiResponse.success) {
+          return apiResponse.payload;
+        } else {
+          console.error(apiResponse.message);
+          console.error(apiResponse.errors);
+          return [];
+        }
+      })
+    );
+  }
+}
