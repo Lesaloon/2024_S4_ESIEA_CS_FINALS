f3d314206eb19b22de05efc7ee73fa8f33f1f133
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-05-29 22:16:45 +0200

    refactor: Update app.routes.ts to include new routes for home, tutorial, step, and not found components

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { TutorialComponent } from './components/tutorial/tutorial.component';
+import { StepComponent } from './components/step/step.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+export const routes: Routes = [
+	{
+		path: "/",
+		component: HomeComponent
+	},
+	{
+		path: "/:id",
+		component: TutorialComponent
+	},
+	{
+		path: "/:id/step/:stepId",
+		component: StepComponent
+	},
+	{
+		path: "/*",
+		component: NotFoundComponent
+	}
+];
