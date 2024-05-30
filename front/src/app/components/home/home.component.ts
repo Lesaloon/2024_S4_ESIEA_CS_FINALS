a4f1fb4896d1e9f7226e431f601602f78fea73a0
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-05-30 21:20:04 +0200

    refactor: Update home component to display tutorials dynamically

--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { OnInit } from '@angular/core';
+import { Tutorial } from '../../interfaces/tutorial';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,19 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  data: Tutorial[] = [];
+
+  constructor( private ApiService: ApiService) {}
+  ngOnInit(): void {
+    this.ApiService.getAllTutorials().subscribe((data: Tutorial[]) => {
+      this.data = data;
+      console.log(data);
+    })
+  }
+
+  goToTutorial(id: number) {
+    console.log(id);
+  }
 }
