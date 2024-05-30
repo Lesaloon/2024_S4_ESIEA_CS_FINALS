3b54c339b1286c8e3c553823fc89e9958344d96c
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-05-30 18:48:00 +0200

    refactor: Add toggleNavbar method to NavBarComponent for collapsing and expanding the navbar

--- front/src/app/shared/nav-bar/nav-bar.component.ts
+++ front/src/app/shared/nav-bar/nav-bar.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  isNavbarCollapsed = true;
 
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
 }
