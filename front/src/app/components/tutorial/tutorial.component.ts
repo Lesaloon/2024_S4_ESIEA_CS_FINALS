bc399ba5b5dc39b1015bd2f0dbd2760d813f5f9f
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-06-01 13:48:39 +0200

    refactor: Update home component to use responsive grid layout

--- front/src/app/components/tutorial/tutorial.component.ts
+++ front/src/app/components/tutorial/tutorial.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from '../../interfaces/tutorial';
+import { ApiService } from '../../services/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { DifficultyLevel } from '../../enum/difficulty-level';
+import { TypeOfTutorial } from '../../enum/type-of-tutorial';
 
 @Component({
   selector: 'app-tutorial',
@@ -7,6 +12,41 @@ import { Component } from '@angular/core';
   templateUrl: './tutorial.component.html',
   styleUrl: './tutorial.component.css'
 })
-export class TutorialComponent {
+export class TutorialComponent implements OnInit {
 
+  tutorial: Tutorial = {} as Tutorial;
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.apiService.getTutorial(this.route.snapshot.params['id']).subscribe((data: Tutorial[]) => {
+      this.tutorial = data[0];
+    });
+  }
+
+  getFrenchDifficulty(difficulty: DifficultyLevel): string {
+    switch (difficulty) {
+      case DifficultyLevel.EASY:
+        return 'Facile';
+      case DifficultyLevel.MEDIUM:
+        return 'Moyen';
+      case DifficultyLevel.HARD:
+        return 'Difficile';
+      default:
+        return 'Inconnu';
+    }
+  }
+
+  getFrenchType(type: TypeOfTutorial): string {
+    switch (type) {
+      case TypeOfTutorial.ARTS_AND_CRAFTS:
+        return 'Loisirs créatifs';
+      case TypeOfTutorial.COOKING:
+        return 'Cuisine';
+      case TypeOfTutorial.DIY:
+        return 'Bricolage';
+      default:
+        return 'Inconnu';
+    }
+  }
 }
