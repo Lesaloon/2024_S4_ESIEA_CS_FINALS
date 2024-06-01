de20edcd83f2dbde93c6cdd40500dfdc204f1383
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-06-02 00:29:12 +0200

    refactor: Remove CommonModule import from app.component.ts

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -7,7 +6,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
