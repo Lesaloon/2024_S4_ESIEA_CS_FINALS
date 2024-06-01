a28a9b86a8b0c6a07e1614bc9bb01e145fb1d47e
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-06-01 14:01:35 +0200

    feat: Add create-tutorial and edit-tutorial components

---
+++ front/src/app/components/create-tutorial/create-tutorial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-tutorial',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-tutorial.component.html',
+  styleUrl: './create-tutorial.component.css'
+})
+export class CreateTutorialComponent {
+
+}
---
+++ front/src/app/components/edit-tutorial/edit-tutorial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-tutorial',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-tutorial.component.html',
+  styleUrl: './edit-tutorial.component.css'
+})
+export class EditTutorialComponent {
+
+}
